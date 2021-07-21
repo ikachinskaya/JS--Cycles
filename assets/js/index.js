270c6f72eae6f0ba141b2ff64efaf9d747748e17
Inna <inna.kachinskaya13@gmail.com>
2021-07-21 23:17:29 +0300

    Add task2

--- assets/js/index.js
+++ assets/js/index.js
@@ -21,3 +21,33 @@ do {
   count--;
 } while (count >= 0);
 console.log("==================");
+
+console.log("Задание 2");
+console.log("for");
+for (let i = 10; i <= 50; i++) {
+  if (i % 5 === 0) {
+    console.log(i);
+  }
+}
+
+console.log("while");
+let counter1 = 10;
+while (true) {
+  if (counter1 % 5 === 0) {
+    console.log(counter1);
+  }
+  counter1++;
+  if (counter1 > 50) {
+    break;
+  }
+}
+
+console.log("do while");
+let count1 = 10;
+do {
+  if (count1 % 5 === 0) {
+    console.log(count1);
+  }
+  count1++;
+} while (count1 <= 50);
+console.log("==================");
